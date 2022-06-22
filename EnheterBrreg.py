378726e1a0c3a234c313329d8b6a7b28a4c5c01e
Joergen Jore <joergenjore@gmail.com>
2022-06-22 14:59:33 +0200

    s

--- EnheterBrreg.py
+++ EnheterBrreg.py
@@ -1,17 +1,9 @@
-from urllib import response
+
 import json
-from pydantic import EmailError
-import requests
-from sqlalchemy import null
-import sql_app
-from sql_app import database
-from sql_app.crud import create_Company
 from sql_app.models import Company
-from sqlalchemy import Column, Integer, Boolean, String
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import sqlite3
+
+
+
 
 def enheter_json():
 
